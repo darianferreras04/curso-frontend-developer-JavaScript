2fcc3a7614afcc14a1df0f53cdec7843dd06263f
Darian Ferreras <darianferreras04@gmail.com>
2023-03-25 02:28:02 -0500

    modificando el carrito de compras

--- main.js
+++ main.js
@@ -2,17 +2,46 @@ const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
 const menuBurger = document.querySelector(".menu");
 const mobileMenu = document.querySelector(".mobile-menu");
+const CarritoIcon = document.querySelector(".navbar-shopping-cart")
+const aside = document.querySelector(".product-detail")
 
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
 
 menuBurger.addEventListener("click", toggleMobileMenu);
 
+CarritoIcon.addEventListener("click", toggleCarritoIcon )
+
 function toggleDesktopMenu(){
-desktopMenu.classList.toggle("inactive")
+
+const isAsideClosed = aside.classList.contains("inactive");
+
+if (!isAsideClosed){
+    aside.classList.add("inactive");
+}
+
+    desktopMenu.classList.toggle("inactive")
 }
 
 function toggleMobileMenu(){
+    
+    const isAsideClosed = aside.classList.contains("inactive");
+
+    if (!isAsideClosed){
+        aside.classList.add("inactive");
+    }
+    
     mobileMenu.classList.toggle("inactive")
     }
  
+function toggleCarritoIcon(){
+    
+    const isMobileMenuClosed = mobileMenu.classList.contains ("inactive");
+
+    if (!isMobileMenuClosed){
+        mobileMenu.classList.add("inactive")
+    }
+    
+    aside.classList.toggle("inactive")
+}
+
